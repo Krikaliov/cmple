103b49c03aacae5ace47b7b613fe4b49bdee6306
Krikaliov <hjvsvminecraft@gmail.com>
2026-05-23 22:17:07 +0200

    #7 (wip 8)

--- tests/testing_maths/src/fact.c
+++ tests/testing_maths/src/fact.c
@@ -1,21 +1,20 @@
 #include "fact.h"
 
-int fact_recursive(int n)
-{
-  if (n < 0) return -1;
-  if (n < 1) return 1;
-  if (n < 3) return n;
-
-  return n * fact_recursive(n-1);
-}
-
-int fact_imperative(int n)
+int fact_recursive(const int n)
 {
   if (n < 0) return -1;
   if (n < 2) return 1;
 
-  int i = n;
-  int x = n;
+  return n * fact_recursive(n-1);
+}
+
+int fact_imperative(const int n)
+{
+  if (n < 0) return -1;
+  if (n < 2) return 1;
+
+  register int i = n;
+  register int x = n;
 
   while (--i) x *= i;
 
--- tests/testing_maths/src/fact.h
+++ tests/testing_maths/src/fact.h
@@ -11,7 +11,7 @@ extern "C" {
  * @param n positive integer
  * @return n! or -1 if the given integer n is negative (<0)
  */
-int fact_recursive(int n);
+int fact_recursive(const int n);
 
 /**
  * Compute the factorial number of any positive integer
@@ -19,7 +19,7 @@ int fact_recursive(int n);
  * @param n positive integer
  * @return n! or -1 if the given integer n is negative (<0)
  */
-int fact_imperative(int n);
+int fact_imperative(const int n);
 
 #ifdef __cplusplus
 }
